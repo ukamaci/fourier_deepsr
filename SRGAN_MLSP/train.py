035e2aa0bb21f165b765617cbd87ea7451ff7583
Berk Iskender <berk.iskender9@gmail.com>
2020-11-16 14:29:50 -0600

    cpu memory issue temporariliy solved

--- SRGAN_MLSP/train.py
+++ SRGAN_MLSP/train.py
@@ -17,13 +17,11 @@ from model import Generator, Discriminator
 
 from torch import nn
 
-
 parser = argparse.ArgumentParser(description='Train Super Resolution Models')
 parser.add_argument('--crop_size', default=88, type=int, help='training images crop size')
 parser.add_argument('--upscale_factor', default=4, type=int, choices=[2, 4, 8],
                     help='super resolution upscale factor')
 parser.add_argument('--num_epochs', default=100, type=int, help='train epoch number')
-
 
 if __name__ == '__main__':
     opt = parser.parse_args()
@@ -111,60 +109,61 @@ if __name__ == '__main__':
         out_path = '/mnt/spinner/mlsp_project/outputs/SRF_4/training_results/training_results/SRF_' + str(UPSCALE_FACTOR) + '/'
         if not os.path.exists(out_path):
             os.makedirs(out_path)
-        
-        with torch.no_grad():
-            val_bar = tqdm(val_loader)
-            valing_results = {'mse': 0, 'ssims': 0, 'psnr': 0, 'ssim': 0, 'batch_sizes': 0}
-            val_images = []
-            for val_lr, val_hr_restore, val_hr in val_bar:
-                batch_size = val_lr.size(0)
-                valing_results['batch_sizes'] += batch_size
-                lr = val_lr
-                hr = val_hr
-                if torch.cuda.is_available():
-                    lr = lr.cuda()
-                    hr = hr.cuda()
-                sr = netG(lr)
-        
-                batch_mse = ((sr - hr) ** 2).data.mean()
-                valing_results['mse'] += batch_mse * batch_size
-                batch_ssim = pytorch_ssim.ssim(sr, hr).item()
-                valing_results['ssims'] += batch_ssim * batch_size
-                valing_results['psnr'] = 10 * log10((hr.max()**2) / (valing_results['mse'] / valing_results['batch_sizes']))
-                valing_results['ssim'] = valing_results['ssims'] / valing_results['batch_sizes']
-                val_bar.set_description(
-                    desc='[converting LR images to SR images] PSNR: %.4f dB SSIM: %.4f' % (
-                        valing_results['psnr'], valing_results['ssim']))
-        
-                val_images.extend(
-                    [display_transform()(val_hr_restore.squeeze(0)), display_transform()(hr.data.cpu().squeeze(0)),
-                     display_transform()(sr.data.cpu().squeeze(0))])
-            val_images = torch.stack(val_images)
-            val_images = torch.chunk(val_images, val_images.size(0) // 15)
-            val_save_bar = tqdm(val_images, desc='[saving training results]')
-            index = 1
-            if epoch % 100 == 0:
-                for image in val_save_bar:
-                    image = utils.make_grid(image, nrow=3, padding=5)
-                    utils.save_image(image, out_path + 'epoch_%d_index_%d.png' % (epoch, index), padding=5)
-                    index += 1
-    
-        # save model parameters
-        if epoch % 50 == 0:
-            torch.save(netG.state_dict(), 'epochs/netG_epoch_%d_%d.pth' % (UPSCALE_FACTOR, epoch))
-            torch.save(netD.state_dict(), 'epochs/netD_epoch_%d_%d.pth' % (UPSCALE_FACTOR, epoch))
-        # save loss\scores\psnr\ssim
-        results['d_loss'].append(running_results['d_loss'] / running_results['batch_sizes'])
-        results['g_loss'].append(running_results['g_loss'] / running_results['batch_sizes'])
-        results['d_score'].append(running_results['d_score'] / running_results['batch_sizes'])
-        results['g_score'].append(running_results['g_score'] / running_results['batch_sizes'])
-        results['psnr'].append(valing_results['psnr'])
-        results['ssim'].append(valing_results['ssim'])
-    
-        if epoch % 10 == 0 and epoch != 0:
-            out_path = 'statistics/'
-            data_frame = pd.DataFrame(
-                data={'Loss_D': results['d_loss'], 'Loss_G': results['g_loss'], 'Score_D': results['d_score'],
-                      'Score_G': results['g_score'], 'PSNR': results['psnr'], 'SSIM': results['ssim']},
-                index=range(1, epoch + 1))
-            data_frame.to_csv(out_path + 'srf_' + str(UPSCALE_FACTOR) + '_train_results.csv', index_label='Epoch')
+
+        if epoch % 1 == 0:
+            with torch.no_grad():
+                val_bar = tqdm(val_loader)
+                valing_results = {'mse': 0, 'ssims': 0, 'psnr': 0, 'ssim': 0, 'batch_sizes': 0}
+                val_images = []
+                for val_lr, val_hr_restore, val_hr in val_bar:
+                    batch_size = val_lr.size(0)
+                    valing_results['batch_sizes'] += batch_size
+                    lr = val_lr
+                    hr = val_hr
+                    if torch.cuda.is_available():
+                        lr = lr.cuda()
+                        hr = hr.cuda()
+                    sr = netG.eval()(lr)
+
+                    batch_mse = ((sr - hr) ** 2).data.mean()
+                    valing_results['mse'] += batch_mse * batch_size
+                    batch_ssim = pytorch_ssim.ssim(sr, hr).item()
+                    valing_results['ssims'] += batch_ssim * batch_size
+                    valing_results['psnr'] = 10 * log10((hr.max()**2) / (valing_results['mse'] / valing_results['batch_sizes']))
+                    valing_results['ssim'] = valing_results['ssims'] / valing_results['batch_sizes']
+                    val_bar.set_description(
+                        desc='[converting LR images to SR images] PSNR: %.4f dB SSIM: %.4f' % (
+                            valing_results['psnr'], valing_results['ssim']))
+
+                    # val_images.extend(
+                    #     [display_transform()(val_hr_restore.squeeze(0)), display_transform()(hr.data.cpu().squeeze(0)),
+                    #      display_transform()(sr.data.cpu().squeeze(0))])
+                # val_images = torch.stack(val_images)
+                # val_images = torch.chunk(val_images, val_images.size(0) // 15)
+                # val_save_bar = tqdm(val_images, desc='[saving training results]')
+                # index = 1
+                # if epoch % 100 == 0:
+                #     for image in val_save_bar:
+                #         image = utils.make_grid(image, nrow=3, padding=5)
+                #         utils.save_image(image, out_path + 'epoch_%d_index_%d.png' % (epoch, index), padding=5)
+                #         index += 1
+
+            # save model parameters
+            if epoch % 50 == 0:
+                torch.save(netG.state_dict(), 'epochs/netG_epoch_%d_%d.pth' % (UPSCALE_FACTOR, epoch))
+                torch.save(netD.state_dict(), 'epochs/netD_epoch_%d_%d.pth' % (UPSCALE_FACTOR, epoch))
+            # save loss\scores\psnr\ssim
+            results['d_loss'].append(running_results['d_loss'] / running_results['batch_sizes'])
+            results['g_loss'].append(running_results['g_loss'] / running_results['batch_sizes'])
+            results['d_score'].append(running_results['d_score'] / running_results['batch_sizes'])
+            results['g_score'].append(running_results['g_score'] / running_results['batch_sizes'])
+            results['psnr'].append(valing_results['psnr'])
+            results['ssim'].append(valing_results['ssim'])
+
+            if epoch % 10 == 0 and epoch != 0:
+                out_path = 'statistics/'
+                data_frame = pd.DataFrame(
+                    data={'Loss_D': results['d_loss'], 'Loss_G': results['g_loss'], 'Score_D': results['d_score'],
+                          'Score_G': results['g_score'], 'PSNR': results['psnr'], 'SSIM': results['ssim']},
+                    index=range(1, epoch + 1))
+                data_frame.to_csv(out_path + 'srf_' + str(UPSCALE_FACTOR) + '_train_results.csv', index_label='Epoch')
